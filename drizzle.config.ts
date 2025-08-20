431e164955cd5736af1bb1e1ff22ad7bd43f7a7e
zuice <jeff.allen127@gmail.com>
2025-08-19 22:21:48 -0400

    Add turso key to drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/lib/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
+    authToken: process.env.TURSO_KEY!,
   },
 });
--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,9 +3,6 @@ import { drizzle } from "drizzle-orm/libsql";
 import { env } from "@/lib/env";
 import * as schema from "@/lib/db/schema";
 
-// log for prod
-console.log(env.DB_FILE_NAME, env.TURSO_KEY);
-
 export const db = drizzle({
   schema,
   connection: {
